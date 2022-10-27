f2e0e2ce300d50692f252eb8cc39bbacb6f27608
sadig2 <clanzu2@yahoo.com>
2022-10-28 01:46:22 +0200

    linting

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ from chess import bishop, knight, queen
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     n: int
-    chess_pieces : str
- 
+    chess_pieces: str
 
-functions_dict = {
-    "queen":queen,
-    "knight":knight,
-    "bishop":bishop,
-    "knight":knight
+
+handlers = {
+    "queen": queen,
+    "knight": knight,
+    "bishop": bishop,
+    "knight": knight
 }
 
 
 @app.post("/")
 def post_item(item: Item):
     chess_piece = item.chess_pieces
-    function = functions_dict[chess_piece]
+    function = handlers[chess_piece]
     solutionsCount = function(item.n)
     result = {}
-    result["solution_count"]=solutionsCount
+    result["solution_count"] = solutionsCount
     return result
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- tests.py
+++ tests.py
@@ -1,24 +1,24 @@
 import unittest
 from chess import queen, rook, bishop, knight
 
+
 class FunctionTest(unittest.TestCase):
 
     def test_queen(self):
         result = queen(2)
-        self.assertEqual(0,result)
+        self.assertEqual(0, result)
 
     def test_rook(self):
         result = rook(2)
-        self.assertEqual(4,result)
+        self.assertEqual(4, result)
 
     def test_bishop(self):
         result = bishop(2)
-        self.assertEqual(8,result)
+        self.assertEqual(8, result)
 
-    
     def test_knight(self):
         result = knight(2)
-        self.assertEqual(16,result)
+        self.assertEqual(16, result)
 
 
 if __name__ == '__main__':
